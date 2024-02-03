8fb5673909892325568d25fc176ab37a3f9e586c
DESKTOP-LKCI9L4 <emily.pn@sisma.org.il>
2024-02-03 15:38:25 +0200

    design

--- app/src/main/java/com/example/emilypanfilovgame/MainActivity.java
+++ app/src/main/java/com/example/emilypanfilovgame/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.emilypanfilovgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
+    private TextView tv4;
+    private TextView tv5;
+    private TextView tv6;
+    private EditText et1;
+    private EditText et2;
+    private EditText et3;
+    private ImageView iv1;
+    private ImageView iv2;
+    private ImageView iv3;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv1=findViewById(R.id.tv1);
+        tv2=findViewById(R.id.tv2);
+        tv3=findViewById(R.id.tv3);
+        tv4=findViewById(R.id.tv4);
+        tv5=findViewById(R.id.tv5);
+        tv6=findViewById(R.id.tv6);
+        et1=(EditText)findViewById(R.id.et1);
+        et2=(EditText)findViewById(R.id.et2);
+        et3=(EditText)findViewById(R.id.et3);
+        iv1=(ImageView) findViewById(R.id.iv1);
+        iv2=(ImageView) findViewById(R.id.iv2);
+        iv3=(ImageView) findViewById(R.id.iv3);
+        btn1=(Button) findViewById(R.id.btn1);
+        btn2=(Button) findViewById(R.id.btn2);
+        btn3=(Button) findViewById(R.id.btn3);
+        btn4=(Button) findViewById(R.id.btn4);
+        btn1.setBackgroundColor(Color.rgb(30,210,230));
+        btn2.setBackgroundColor(Color.rgb(30,210,230));
+        btn3.setBackgroundColor(Color.rgb(30,210,230));
+        btn4.setBackgroundColor(Color.rgb(220,80,250));
+
+
     }
 }
